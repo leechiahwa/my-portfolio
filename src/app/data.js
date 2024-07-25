1283234c1f79e93276edc6954482e4e5b29b6791
leechiahwa <leechiahwa99@hotmail.com>
2024-07-25 18:51:54 +0800

    Initial commit

---
+++ src/app/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "To be added",
+    subtitle: "Python",
+    description:
+      "This is a project I built using Python.",
+    image: "./project-1.jpg",
+    link: ""
+  },
+  {
+    title: "To be added",
+    subtitle: "Python",
+    description:
+      "This is a project I built using Python.",
+    image: "./project-1.jpg",
+    link: ""
+  }
+];
+
+export const skills = [
+  "Python",
+  "JavaScript",
+  "React",
+  "HTML",
+  "CSS",
+  "Tailwind",
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Melvin is a great developer. He has helped me with my project and he is very knowledgeable.",
+    image: "https://randomuser.me/api/portraits/women/74.jpg",
+    name: "Jenny Wilson",
+    company: "Google",
+  },
+  {
+    quote:
+      "Melvin is a great developer. He has helped me with my project and he is very knowledgeable.",
+    image: "https://randomuser.me/api/portraits/women/76.jpg",
+    name: "Jenny Wilson",
+    company: "Facebook",
+  }
+];
