38671d5af1055d862fca6ec4710a01810b38b9c6
leechiahwa <leechiahwa99@hotmail.com>
2024-10-04 17:39:25 +0800

    Feat: Add scrolling animation

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,9 +1,9 @@
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
+    <header className="bg-gray-800 md:sticky top-0">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <div className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
@@ -23,11 +23,12 @@ export default function Navbar() {
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
     </header>
-  )
+  );
 }
